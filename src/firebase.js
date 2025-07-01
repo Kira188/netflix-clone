b4cf3fe489db3cada9e1b42217428b3bc1c1b4e1
Kira476 <Darkhorizon476@gmail.com>
2025-07-01 18:51:08 +0530

    final changes

--- src/firebase.js
+++ src/firebase.js
@@ -7,16 +7,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-  apiKey: "AIzaSyCTIbs7dX4K-X_duHvNUoXCmcFuVFOWlfg",
+const firebaseConfig = {
+  apiKey: "AIzaSyDbAPTDWObgfPNfS9HfNy0rUMJf5_k_0W0",
   authDomain: "netflix-clone-9a54a.firebaseapp.com",
+  databaseURL: "https://netflix-clone-9a54a-default-rtdb.firebaseio.com",
   projectId: "netflix-clone-9a54a",
   storageBucket: "netflix-clone-9a54a.firebasestorage.app",
   messagingSenderId: "333628603609",
-  appId: "1:333628603609:web:d1d85f72c69127438e12c8",
-  measurementId: "G-HE5H3Z8T4S"
+  appId: "1:333628603609:web:6799c2bc6ae08bc28e12c8",
+  measurementId: "G-E6YDFNRKXX"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
